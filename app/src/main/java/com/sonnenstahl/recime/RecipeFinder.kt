e42102bb187ee5f7b58b458587bc81bbb152d180
Nik Tsonev <nikolay.l.tsonev@gmail.com>
2025-07-19 16:19:06 +0200

    added now the labels and mutual exlcusion between meats and vegan

--- app/src/main/java/com/sonnenstahl/recime/RecipeFinder.kt
+++ app/src/main/java/com/sonnenstahl/recime/RecipeFinder.kt
@@ -1,34 +1,124 @@
 package com.sonnenstahl.recime
 
-import androidx.compose.foundation.layout.Arrangement
+import android.util.Log
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.sonnenstahl.recime.utils.data.MEAT_OPTION_NAMES
+import com.sonnenstahl.recime.utils.data.MeatOption
+import com.sonnenstahl.recime.utils.data.disableMeat
+import com.sonnenstahl.recime.utils.data.disableVegan
+import com.sonnenstahl.recime.utils.data.excludedNames
+import com.sonnenstahl.recime.utils.data.initMeatOptions
 
 @Composable
 fun RecipeFinder(navController: NavController) {
-    var mealName by remember { mutableStateOf("Hello Worlds") }
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier =
-            Modifier
-                .fillMaxSize(),
-    ) {
-        TextField(
-            value = mealName,
-            onValueChange = {mealName = it }
-        )
+    var mealName by remember { mutableStateOf("") }
+    var meatOptions by remember { mutableStateOf(emptyList<MeatOption>()) }
 
-        Text("hello World")
+    LaunchedEffect(Unit) {
+        meatOptions = initMeatOptions()
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Log.d("MEAT OPTIONS", "$meatOptions")
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "Find a meal",
+                style = MaterialTheme.typography.headlineMedium,
+                modifier = Modifier.padding(vertical = 10.dp)
+            )
+
+            TextField(
+                value = mealName,
+                singleLine = true,
+                onValueChange = { mealName = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            )
+
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+                userScrollEnabled = false
+            ) {
+                items(meatOptions.size) { index ->
+                    val currentOption = meatOptions[index]
+                    val isChosen = meatOptions[index].isChosen.value
+
+                    if (isChosen == true ) {
+                        if (currentOption.name in excludedNames.take(2)) {
+                            disableMeat(meatOptions)
+                        }
+                        // only the meats
+                        val meats = MEAT_OPTION_NAMES
+                            .take(MEAT_OPTION_NAMES.size-2)
+                            .filter { it != "Pasta" }
+
+                        if (currentOption.name in meats) {
+                            disableVegan(meatOptions)
+                        }
+                    }
+
+                    OutlinedButton(
+                        onClick = {
+                            meatOptions[index].isChosen.value = !isChosen
+                        },
+                        modifier = Modifier
+                            .padding(6.dp)
+                            .fillMaxWidth(),
+                        shape = RoundedCornerShape(12.dp),
+                        border = BorderStroke(2.dp, MaterialTheme.colorScheme.primary),
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            containerColor = if (isChosen) MaterialTheme.colorScheme.primary.copy(alpha = 0.1f) else MaterialTheme.colorScheme.surface,
+                            contentColor = if (isChosen) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.onSurface
+                        )
+                    ) {
+                        Text(
+                            text = currentOption.name,
+                            modifier = Modifier.padding(8.dp),
+                            fontSize = 15.sp
+
+                        )
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/sonnenstahl/recime/utils/data/MeatOption.kt
+++ app/src/main/java/com/sonnenstahl/recime/utils/data/MeatOption.kt
@@ -1,5 +1,8 @@
 package com.sonnenstahl.recime.utils.data
 
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
+
 val MEAT_OPTION_NAMES: Array<String> = arrayOf(
     "Beef",
     "Chicken",
@@ -12,9 +15,22 @@ val MEAT_OPTION_NAMES: Array<String> = arrayOf(
     "Vegetarian"
 )
 
+val excludedNames = setOf("Vegan", "Vegetarian", "Pasta")
+
 data class MeatOption (
     val name: String,
-    var isChosen: Boolean
+    var isChosen: MutableState<Boolean> = mutableStateOf(false)
 )
 
-fun initMeatOptions(): List<MeatOption> = MEAT_OPTION_NAMES.map { MeatOption(it, false) }
+fun initMeatOptions(): List<MeatOption> = MEAT_OPTION_NAMES.map { MeatOption(it) }
+
+fun disableMeat(meatOptions: List<MeatOption>) =
+    meatOptions
+        .filter { it.name !in excludedNames}
+        .forEach { it.isChosen.value = false }
+
+fun disableVegan(meatOptions: List<MeatOption>) =
+    meatOptions
+        .filter { it.name in excludedNames.take(2) }
+        .forEach { it.isChosen.value = false }
+
